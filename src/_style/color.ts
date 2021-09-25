493983808cad3e8c24617c38ed83455dfadf6d24
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-09-25 14:05:46 +0700

    feat: Add theme

---
+++ src/_style/color.ts
@@ -0,0 +1,15 @@
+import { defaultTheme, generateHexAlphaVariants } from '@xstyled/styled-components';
+
+const color = {
+  ...defaultTheme.colors,
+  ...generateHexAlphaVariants({
+    watermelon: "#fd4f5d",
+    lemon: "#FED330",
+    skyblue: "#1cb7fc",
+    emerald: "#00b900",
+    tangerine: "#ff9500",
+    "dust-black": "#25282B",
+  })
+};
+
+export default color;
---
+++ src/_style/theme.ts
@@ -0,0 +1,33 @@
+import { defaultTheme } from "@xstyled/styled-components";
+import color from "./color";
+
+const fontSizes = {
+  ...defaultTheme.fontSizes,
+  xxs: "9px",
+  "extra-small": "11px",
+  small: "14px",
+  regular: "16px",
+  h6: "20px",
+  h5: "24px",
+  h4: "28px",
+  h3: "32px",
+  h2: "40px",
+  h1: "48px"
+}
+
+const fonts = {
+  ...defaultTheme.fonts,
+  sans: "'Google Sans', system-ui,-apple-system,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji'",
+  display: "'Google Sans', system-ui,-apple-system,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji'",
+  body: "'Google Sans', system-ui,-apple-system,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji'",
+}
+
+const theme = {
+  ...defaultTheme,
+  colors: {
+    ...color
+  },
+  fontSizes,
+};
+
+export default theme;
