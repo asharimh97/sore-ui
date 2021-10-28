196a92be11235b6a64474cb6f35deebba76a82c8
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-10-28 22:28:23 +0700

    feat: Update styled components declaration

--- @types/styled-components.d.ts
+++ @types/styled-components.d.ts
@@ -1,10 +1,21 @@
+// styled.d.ts
+
 // import original module declarations
-import "styled-components";
-import { DefaultTheme as XStyledDefaultTheme } from "@xstyled/styled-components";
+import 'styled-components'
+import '@xstyled/system'
+import {
+  ITheme,
+  DefaultTheme as XStyledDefaultTheme,
+} from '@xstyled/styled-components'
+
+interface AppTheme extends ITheme, XStyledDefaultTheme {
+  /* Customize your theme */
+}
 
 // and extend them!
-declare module "styled-components" {
-  export interface DefaultTheme extends XStyledDefaultTheme {
-    /* Customize your theme */
-  }
+declare module '@xstyled/system' {
+  export interface Theme extends AppTheme {}
+}
+declare module 'styled-components' {
+  export interface DefaultTheme extends AppTheme {}
 }
