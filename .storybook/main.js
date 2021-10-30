4de90c5da130018ebdfe706fd174dcd544271583
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-10-30 15:10:59 +0700

    Fix build storybook problem
    Somehow there is a problem regarding core-js about object assign,
    thus we add some patch into `main.js`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,22 @@
+const path = require("path");
+
+/**
+ * Core JS problem on build resolve reference:
+ * https://github.com/storybookjs/storybook/issues/11255#issuecomment-673899817
+ */
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  resolve: {
+    alias: {
+      "core-js/modules": path.resolve(
+        __dirname,
+        "..",
+        "node_modules/core-js/modules",
+      ),
+    },
+  },
+};
