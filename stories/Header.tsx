95b4058c5742b6736f3498949631d32e89e0d666
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-10-30 15:11:27 +0700

    Update storybook implementation

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import Base from "../src/Common/Base";
+import { Base, Flex, Grid } from "../src/Common";
 
 import { Button } from "./Button";
 import "./header.css";
+
+import { flexProps } from "../src/utils";
 
 interface HeaderProps {
   user?: {};
@@ -12,40 +14,67 @@ interface HeaderProps {
 }
 
 export const Header = ({
-  user, onLogin, onLogout, onCreateAccount,
-}: HeaderProps) => (
-  <header>
-    <div className="wrapper">
-      <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <path
-              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-              fill="#FFF"
-            />
-            <path
-              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-              fill="#555AB9"
-            />
-            <path
-              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill="#91BAF8"
-            />
-          </g>
-        </svg>
-        <h1>Acme</h1>
+  user,
+  onLogin,
+  onLogout,
+  onCreateAccount,
+}: HeaderProps) => {
+  return (
+    <header>
+      <div className="wrapper">
+        <div>
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fillRule="evenodd">
+              <path
+                d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+                fill="#FFF"
+              />
+              <path
+                d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+                fill="#555AB9"
+              />
+              <path
+                d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+                fill="#91BAF8"
+              />
+            </g>
+          </svg>
+          <h1>Acme</h1>
+        </div>
+        <div>
+          {user ? (
+            <Button size="small" onClick={onLogout} label="Log out" />
+          ) : (
+            <>
+              <Button size="small" onClick={onLogin} label="Log in" />
+              <Button
+                primary
+                size="small"
+                onClick={onCreateAccount}
+                label="Sign up"
+              />
+            </>
+          )}
+        </div>
       </div>
-      <div>
-        {user ? (
-          <Button size="small" onClick={onLogout} label="Log out" />
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
-      </div>
-    </div>
-    <Base.p color="tangerine-a50" py="10">Hohoho ini dibikin dari common component</Base.p>
-  </header>
-);
+      <Base.p color="tangerine-a50" py="10">
+        Hohoho ini dibikin dari common component
+      </Base.p>
+      <Flex justify="space-between">
+        <p>Hohoho apakah aku bisa di samping kiri?</p>
+        <p>Hohoho apakah aku bisa di tengah?</p>
+        <p>Hohoho apakah aku bisa di samping kanan?</p>
+      </Flex>
+      <Grid column="repeat(3, 1fr)">
+        <p>Hohoho apakah aku bisa di samping kiri?</p>
+        <p>Hohoho apakah aku bisa di tengah?</p>
+        <p>Hohoho apakah aku bisa di samping kanan?</p>
+      </Grid>
+    </header>
+  );
+};
