2bcbf2d8c81026a25bed9c68a035de3076f33ba0
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-10-31 21:49:37 +0700

    Add text component

---
+++ src/Common/Text.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createCss, compose, ITheme, Theme } from "@xstyled/styled-components";
+import { SystemStyleProps, systemProps } from "../config/xstyled.config";
+import {
+  TextProps as CustomTextProps,
+  textProps as customTextProps,
+} from "../utils/text-props";
+
+interface TextProps<T extends ITheme = Theme>
+  extends Omit<SystemStyleProps<T>, "size" | "height">,
+    CustomTextProps<T> {}
+
+const textProps = compose<TextProps>(systemProps, customTextProps);
+
+const { x } = createCss(textProps);
+
+const Text: React.FC<TextProps> = (props) => {
+  return <x.p {...props} />;
+};
+
+export default Text;
