cd17ad428cc7aac10870787bf9a7ef9b6acdd0a8
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-11-09 22:28:47 +0700

    Add Divider with text

--- src/Common/Divider.tsx
+++ src/Common/Divider.tsx
@@ -1,4 +1,7 @@
-import { x, styled } from "../config/xstyled.config";
+import React from "react";
+import { ITheme, Theme } from "@xstyled/styled-components";
+import { x, styled, SystemStyleProps } from "../config/xstyled.config";
+import Text from "./Text";
 
 const { div } = x;
 
@@ -9,11 +12,68 @@ const { div } = x;
  * - align/orientation
  */
 
-const Divider = styled(x.div)`
-  border-top: solid 1px cool-gray-300;
+const SimpleDivider = styled(x.div)`
+  border-top: solid 1px;
+  border-color: cool-gray-300;
   height: 1px;
+  margin: 16px 0;
   width: 100%;
   min-width: 100%;
 `;
+
+const TextWrapper = styled(Text)`
+  color: cool-gray-700;
+  font-size: 14px;
+  padding: 0 1em;
+`;
+
+const DividerWrapper = styled(x.div)`
+  align-items: center;
+  clear: both;
+  display: flex;
+  justify-content: center;
+  margin: 16px 0;
+  position: relative;
+  width: 100%;
+  min-width: 100%;
+
+  &:before {
+    content: "";
+    border-top: solid 1px;
+    border-color: cool-gray-300;
+    height: 1px;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    position: relative;
+    width: 50%;
+  }
+
+  &:after {
+    content: "";
+    border-top: solid 1px;
+    border-color: cool-gray-300;
+    height: 1px;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    position: relative;
+    width: 50%;
+  }
+`;
+
+interface DividerProps<T extends ITheme = Theme> extends SystemStyleProps<T> {}
+
+const Divider: React.FC<DividerProps> = ({ children, ...props }) => {
+  if (!children) {
+    return <SimpleDivider {...props} />;
+  }
+
+  return (
+    <DividerWrapper>
+      <TextWrapper>{children}</TextWrapper>
+    </DividerWrapper>
+  );
+};
 
 export default Divider;
--- src/Common/index.ts
+++ src/Common/index.ts
@@ -1,5 +1,6 @@
 export { default as Base } from "./Base";
 export { default as Box } from "./Box";
+export { default as Divider } from "./Divider";
 export { default as Flex } from "./Flex";
 export { default as Grid } from "./Grid";
 export { default as Image } from "./Image";
