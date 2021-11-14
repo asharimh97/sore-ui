df528ef4895ac6fcf477bc3a6c1aef57a537ae2b
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-11-14 14:06:13 +0700

    Update component props

--- src/utils/button-props.ts
+++ src/utils/button-props.ts
@@ -1,11 +1,6 @@
+import React from "react";
 import * as CSS from "csstype";
-import {
-  style,
-  SystemProp,
-  Theme,
-  ITheme,
-  compose,
-} from "@xstyled/styled-components";
+import { style, Theme, ITheme, compose } from "@xstyled/styled-components";
 
 /**
  * Button props
@@ -30,7 +25,29 @@ const block = style<BlockProps>({
   }),
 });
 
+// styled in css
+interface SizeProps {
+  size?: "small" | "medium" | "large";
+}
+
+interface IconProps {
+  icon?: React.ReactNode;
+}
+
 // eslint-disable-next-line no-unused-vars
-export interface ButtonProps<T extends ITheme = Theme> extends BlockProps {}
+export interface ButtonProps<T extends ITheme = Theme>
+  extends BlockProps,
+    SizeProps,
+    IconProps {
+  primary?: boolean;
+  secondary?: boolean;
+  ghost?: boolean;
+  link?: boolean;
+  text?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  htmlType?: string;
+}
 
 export const buttonProps = compose(block);
--- src/utils/general-props.ts
+++ src/utils/general-props.ts
@@ -6,6 +6,7 @@ import {
   Theme,
   ITheme,
   compose,
+  getShadow,
 } from "@xstyled/styled-components";
 
 interface HeightProps<T extends ITheme = Theme> {
@@ -28,9 +29,20 @@ interface AsProps {
   forwardedAs?: string | React.ComponentType<any>;
 }
 
+interface ShadowProps<T extends ITheme = Theme> {
+  // shadow?: "sm" | "default" | "md" | "lg" | "xl" | "2xl";
+  shadow?: SystemProp<CSS.Property.BoxShadow, T>;
+}
+const shadow = style<ShadowProps>({
+  prop: "shadow",
+  css: "boxShadow",
+  themeGet: getShadow,
+});
+
 export interface GeneralProps<T extends ITheme = Theme>
   extends HeightProps<T>,
     WidthProps<T>,
+    ShadowProps<T>,
     AsProps {}
 
-export const generalProps = compose<GeneralProps>(height, width);
+export const generalProps = compose<GeneralProps>(height, width, shadow);
