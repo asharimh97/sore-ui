e25097c8f2c28b26de05fc8a9f476409df0d38ca
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-11-22 05:42:41 +0700

    Add index as main source library

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Global styles and default theme
+export { default as Preflight } from "./_style/GlobalStyle";
+export { default as DefaultTheme } from "./_style/theme";
+
+// export * as Common from "./Common";
