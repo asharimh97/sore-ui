8559d8e5b176eae77b951d2be88461c93d8d00a7
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-02 09:18:19 +0700

    Update alert implementation
    Add closable method using react icons

--- src/Common/Alert.tsx
+++ src/Common/Alert.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { createCss, compose, ITheme, Theme } from "@xstyled/styled-components";
+import { ITheme, Theme } from "@xstyled/styled-components";
 import { SystemStyleProps, x, styled, css } from "../config/xstyled.config";
 import Text from "./Text";
 import Box from "./Box";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 interface AlertProps<T extends ITheme = Theme>
   extends Omit<SystemStyleProps<T>, "type"> {
   closable?: boolean;
-  message?: React.ReactNode;
+  message: React.ReactNode;
   description?: React.ReactNode;
   type?: "success" | "info" | "warning" | "error";
 }
@@ -19,14 +20,31 @@ const alertColor = {
   error: "red",
 };
 
-const AlertWrapper = styled(x.div)<AlertProps>`
+interface AlertWrapperProps extends Omit<AlertProps, "message"> {
+  visible?: boolean;
+}
+
+const AlertWrapper = styled(x.div)<AlertWrapperProps>`
   align-items: center;
   border: solid 1px;
   color: cool-gray-800;
   display: flex;
   justify-content: space-between;
+  opacity: 0;
   padding: 10px 15px;
+  visibility: hidden;
   width: 100%;
+  transition: all 0.2s ease;
+  -moz-transition: all 0.2s ease;
+  -webkit-transition: all 0.2s ease;
+  -o-transition: all 0.2s ease;
+
+  ${({ visible }) =>
+    visible &&
+    css`
+      opacity: 1;
+      visibility: visible;
+    `}
 
   ${({ type }) =>
     type &&
@@ -34,14 +52,23 @@ const AlertWrapper = styled(x.div)<AlertProps>`
       border-color: ${`${alertColor[type]}-400`};
       background-color: ${`${alertColor[type]}-200-a50`};
     `}
+
+  ${({ description }) =>
+    description &&
+    css`
+      align-items: flex-start;
+    `}
 `;
 
-const Alert = ({
-  type = "info",
-  message,
-  description,
-  ...props
-}: AlertProps) => {
+const IconWrapper = styled(x.span)`
+  color: cool-gray-900-a50;
+  cursor: pointer;
+`;
+
+const Alert = ({ type = "info", message, ...props }: AlertProps) => {
+  const { description, closable } = props;
+
+  const [visible, setVisible] = React.useState<boolean>(true);
   const messageStyle = description
     ? {
         size: "17px",
@@ -50,15 +77,24 @@ const Alert = ({
       }
     : {};
 
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <>
-      <AlertWrapper type={type} {...props}>
+      <AlertWrapper type={type} {...props} visible={visible}>
         <Box>
           <Text size="14px" {...messageStyle}>
             {message}
           </Text>
           {description && <Text size="13px">{description}</Text>}
         </Box>
+        {closable && (
+          <IconWrapper onClick={onClose}>
+            <IoCloseCircleOutline />
+          </IconWrapper>
+        )}
       </AlertWrapper>
     </>
   );
