e2988eb220cfca95f0e51969c46f1fd57ce7cb09
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-02 09:29:39 +0700

    Update export library

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,13 @@
 export { default as Preflight } from "./_style/GlobalStyle";
 export { default as DefaultTheme } from "./_style/theme";
 
-// export * as Common from "./Common";
+// Common components
+export { default as Alert } from "./Common/Alert";
+export { default as Button } from "./Common/Button";
+export { default as Box } from "./Common/Box";
+export { default as Divider } from "./Common/Divider";
+export { default as Flex } from "./Common/Flex";
+export { default as Grid } from "./Common/Grid";
+export { default as Image } from "./Common/Image";
+export { default as Tag } from "./Common/Tag";
+export { default as Text } from "./Common/Text";
