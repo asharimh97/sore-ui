946a0b3a6b3089483ba7ab10d7fb34f5949266b4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-11 19:29:26 +0700

    Update card implementation

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Box } from "../Common";
 import { styled } from "../config/xstyled.config";
+import { SystemStyleProps } from "../config/xstyled.config";
 
-const Card: React.FC = ({ children, ...props }) => {
+const Card: React.FC<SystemStyleProps> = ({ children, ...props }) => {
   return <Box {...props}>{children}</Box>;
 };
 
 Card.defaultProps = {
   p: "8px",
-  boxShadow: "md",
+  boxShadow: "sm",
+  borderRadius: "md",
+  border: "solid 0.5px",
+  borderColor: "blue-gray-100",
 };
 
 export default Card;
---
+++ src/Card/index.tsx
@@ -0,0 +1,10 @@
+import BaseCard from "./Card";
+import BlogCard from "./BlogCard";
+
+const Card = BaseCard;
+
+export const Base = BaseCard;
+
+export const Blog = BlogCard;
+
+export default Card;
